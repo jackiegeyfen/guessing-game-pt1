d0c20467fc56586e752ea39224b28cede9ea9848
vivcolvmn <vivcolvmn@gmail.com>
2023-08-29 21:08:15 -0700

    finished part1

--- guessing-game.js
+++ guessing-game.js
@@ -1,1 +1,33 @@
-hey hey hey
+const readline = require('node:readline');
+const { stdin: input, stdout: output } = require('node:process');
+
+const rl = readline.createInterface({ input, output });
+
+function askGuess() {
+    rl.question('Enter a Guess ', (num) => {
+        checkGuess(num);
+        // console.log()
+        rl.close();
+
+})
+    }
+
+let secretNumber = 10
+
+function checkGuess(num) {
+
+    if (Number(num) < secretNumber) {
+        console.log("Too Low");
+        return false;
+    }
+    if (Number(num) > secretNumber) {
+        console.log("Too High");
+        return false;
+    }
+    if (Number(num) === secretNumber) {
+        console.log("Correct!");
+        return true;
+    }
+}
+
+askGuess()
